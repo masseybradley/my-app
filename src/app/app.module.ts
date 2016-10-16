fbebfb724bb467b1295ca228ff995c6bad3e9f9b
tinytox <massey.bradley@gmail.com>
2016-10-16 22:48:05 +0200

    initial commit includes bootstap dashboard, versions filter and version selection event listener

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// bootstrap
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+// components
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FsaVersionComponent } from './fsa-version/fsa-version.component';
+// services
+import { VersionService } from './shared/version.service';
+import { EntityService } from './shared/entity.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FsaVersionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    VersionService,
+    EntityService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
